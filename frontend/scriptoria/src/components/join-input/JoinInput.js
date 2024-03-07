2c41f9e2d79c5252ccc255e5925407fdefdf1be1
amjad awad <amjadawad129@gmail.com>
2024-03-07 19:57:33 +0200

    style part2

--- frontend/scriptoria/src/components/join-input/JoinInput.js
+++ frontend/scriptoria/src/components/join-input/JoinInput.js
@@ -1,12 +1,9 @@
-
-
 const JoinInput = (props) => {
-    return (
-        <div className="inputBox">
-            <input type="text" required="" className="input-info" />
-            <span >{props.title}</span>
-        </div>
-    )
+    const changeHandler = (event) => {
+        props.method(event.target.value)
+    }
+    return (<div className="inputBox">
+        <input type={props.type} required="" className="input-info" onChange={changeHandler} style={{ borderColor: props.color }} />             <span style={{ color: props.color }} >{props.title}</span>         </div>)
 }
 
 export default JoinInput
--- frontend/scriptoria/src/components/sign-up/SignUp.js
+++ frontend/scriptoria/src/components/sign-up/SignUp.js
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import ConnectButton from "../connect-button/ConnectButton";
 import JoinInput from "../join-input/JoinInput";
+import validator from "validator";
 import "./SignUp.css"
 
 const SignUp = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+    const [user, setUser] = useState({})
+
+    const signUpHandler = () => {
+        let validatorCounter = 0;
+        if (validator.isEmail(email)) {
+            validatorCounter++;
+        }
+        if (password === confirmPassword) {
+            validatorCounter++;
+        }
+        if (validatorCounter !== 2) {
+            setError("Invalid data");
+        }
+        if (error !== "") {
+            setError("")
+            const user = {
+                name, email, password
+            }
+            setUser(user)
+        }
+    }
     return (
         <>
-            <div className="container d-flex justify-content-center align-items-center vh-100">
+            <div className="container-fluid d-flex justify-content-center align-items-center my-3">
                 <div className="row">
-                    <div className="col-lg-6 box-1 d-flex justify-content-center align-items-center text-center">
+                    <div className="col-12 col-lg-6 box-1 d-flex justify-content-center align-items-center text-center">
                         <div>
                             <span className="box1-header">Welcome Back!</span>
                             <p className="box1-text">sign in to continue your creativity with spectoria</p>
@@ -15,17 +43,20 @@ const SignUp = () => {
                         </div>
                         <div className="side-box1"></div>
                     </div>
-                    <div className="col-lg-6 box-2 d-flex flex-column align-items-center text-center">
+                    <div className="col-12 col-lg-6 box-2 d-flex flex-column align-items-center text-center">
                         <div>
                             <span className="box2-header">Create Account</span>
                         </div>
                         <div>
-                            <JoinInput title="name" />
-                            <JoinInput title="email" />
-                            <JoinInput title="password" />
-                            <JoinInput title="confirm password" />
+                            <form>
+                                <JoinInput title="name" method={setName} />
+                                <JoinInput title="email" method={setEmail} />
+                                <JoinInput title="password" method={setPassword} />
+                                <JoinInput title="confirm password" method={setConfirmPassword} />
+                            </form>
                         </div>
-                        <button className="btn signup-btn">Sign Up</button>
+                        <button className="btn signup-btn" onClick={signUpHandler}>Sign Up</button>
+                        <p className="error-text">{error}</p>
                         <div className="hr-div">
                             <hr className="signup-hr" />
                             <span>Or connect with</span>
