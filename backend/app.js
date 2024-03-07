9f777f627146e69fbeef96fc3f70e4735935062b
Omar Khalili <OmarKhalili810@gmail.com>
2024-03-07 21:55:19 +0200

    adding the proxy and cors

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import "./src/DB/mongoose.js";
 import userRouter from "./src/routers/account.js"
-
+import cors from "cors"
 const app = express()
 
 const port = process.env.PORT || 5000
 
-
+app.use(cors());
 app.use(express.json())
 app.use(userRouter)
 
