c4f6d33d7929e22ddd4005852bd7a68656655b1c
mohammad ali jaber <jaberaaa8@gmail.com>
2024-03-10 00:09:32 +0300

    i add my task talken about story detalis

--- frontend/scriptoria/src/App.js
+++ frontend/scriptoria/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './components/sign-up/SignUp';
+import StoryDetails from './components/story-details/StoryDetails';
 
 function App() {
   return (
-    <SignUp />
+   // <SignUp />
+   <StoryDetails/>
   );
 }
 
