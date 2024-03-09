e8ab4afb6c67f502ac6835c3a3cfb79d06090cf2
areenabudayeh <areen.a.abodayeh@gamil.com>
2024-03-09 12:12:00 +0200

    delete file StarRating css and modified StarRating,StoryCard files

--- frontend/scriptoria/src/components/story-card/StoryCard.js
+++ frontend/scriptoria/src/components/story-card/StoryCard.js
@@ -14,6 +14,7 @@ const StoryCard = () => {
             image: 'https://i.pinimg.com/originals/b2/19/21/b2192115221557ec63aaefdcd1e64c4a.jpg',
             description: 'Superman: Chronicles the transformation of Clark Kent into the iconic superhero, highlighting his origin and unwavering commitment to justice.',
             voters: 850,
+            rating:2,
             color:" bg-warning"
         },
         {
@@ -23,6 +24,7 @@ const StoryCard = () => {
             image: 'https://imgix-media.wbdndc.net/ingest/book/preview/510d37e5-c2b6-4430-94c8-ea1aebd8fc2d/affa926f-7434-449d-ad14-0ab0a757b68a/0.jpg',
             description: 'Batman: Follow Bruce Wayne s evolution into the Dark Knight as he battles crime in Gotham.',
             voters: 850,
+            rating:3.5,
             color :"bg-secondary "
         },
         {
@@ -32,6 +34,7 @@ const StoryCard = () => {
             image: 'https://images-na.ssl-images-amazon.com/images/I/31zw0daVfwL._SX331_BO1%2C204%2C203%2C200_.jpg',
             description: 'Solo Leveling" chronicles Jinwoo s growth as a hunter with a unique power in a monster-filled world, catering to fans of action.',
             voters: 850,
+            rating:1.7,
             color :"bg-dark text-white"
         },
         {
@@ -41,6 +44,7 @@ const StoryCard = () => {
             image: 'https://m.media-amazon.com/images/I/51bmUW46V-L.jpg',
             description: 'Superman: Chronicles the transformation of Clark Kent into the iconic superhero, highlighting his origin and unwavering commitment to justice.',
             voters: 850,
+            rating:4.8,
             color :"bg-info"
         },
         {
@@ -50,6 +54,7 @@ const StoryCard = () => {
             image: 'https://imgix-media.wbdndc.net/ingest/book/preview/510d37e5-c2b6-4430-94c8-ea1aebd8fc2d/affa926f-7434-449d-ad14-0ab0a757b68a/0.jpg',
             description: 'Batman: Follow Bruce Wayne s evolution into the Dark Knight as he battles crime in Gotham, revealing the essence of justice and resilience in the iconic superhero.',
             voters: 850,
+            rating:4,
             color :"bg-secondary "
         },
     ];
@@ -64,19 +69,12 @@ const StoryCard = () => {
                                 <div className="col-md-4">
                                     <img src={story.image} alt="" className="card-img" />
                                 </div>
-                                {/*
-                                <div className="col-md-4">
-                                    <Link to={/story/${book.id}}>
-                                    <img src={book.image} alt="" className="card-img" />
-                                    </Link>
-                                </div>
-                                */}
                                 <div className="col-md-8">
                                     <div className="card-body">
                                         <h5 className="card-title text-white  ">{story.title}</h5>
                                         <h6 className="author text-white" >{story.author}</h6>
                                         <div className="d-flex ">
-                                            <StarRating />
+                                            <StarRating rating={story.rating} />
                                             <div className='voters text-white '>{story.voters} votes</div>
                                         </div>
                                         <p className="card-text text-white ">{story.description}</p>
