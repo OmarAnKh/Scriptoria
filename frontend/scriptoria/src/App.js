a53da38cb70016114cc831919accb613f2be1976
Omar Khalili <OmarKhalili810@gmail.com>
2024-03-10 16:07:03 +0200

    editing fixing some errors

--- frontend/scriptoria/src/App.js
+++ frontend/scriptoria/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import SignIn from './components/sign-in/SignIn';
 import { Route, Routes } from 'react-router-dom';
 import SingUp from "./components/sign-up/SignUp";
-import StoryCard from './components/story-card/StoryCard';
 import HomePage from './components/home-page/HomePage';
 function App() {
   return (
