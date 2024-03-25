48464c82d8d7ab61085d7fa97ae6af5d533031b5
mohammad ali jaber <jaberaaa8@gmail.com>
2024-03-25 14:56:29 +0300

    i added my task taken about backend for storyDetails

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import "./src/DB/mongoose.js";
 import userRouter from "./src/routers/account.js"
-
+import storyRouter from "./src/routers/story.js"
 import cors from "cors"
+
 const app = express()
 
 const port = process.env.PORT || 5000
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json())
 app.use(userRouter)
+app.use(storyRouter)
 
 app.listen(port, () => {
     console.log('run on port ' + port)
--- frontend/scriptoria/src/components/story-details/StoryDetails.js
+++ frontend/scriptoria/src/components/story-details/StoryDetails.js
@@ -1,62 +1,125 @@
 import "./StoryDetails.css";
-
 import React, { useState } from 'react'
-import  Languege  from "./Languege";
-import TargetAudience from "./TargetAudience";
-import Category from "./Category";
+import  Languege  from "./details-in-forms/Languege";
+import TargetAudience from "./details-in-forms/TargetAudience";
+import Category from "./details-in-forms/Category";
+import LabelOfStory from "./details-in-forms/LabelOfStory";
+import { Uploader } from "uploader";
+import { UploadButton } from "react-uploader";
+import { account } from "../../api/accountApi"
+const uploader = Uploader({
+  apiKey: "free" 
+});
+
+const options = { multi: true };
+const  htmlForStory=["inputCity","inputPassword4","","inputAddress2","character-background-color"]
+const typeofStory=["text","number","color","textarea"]
+const nameOfDetails=["Title","Number of slides","description (300 word max)","Main Characters","Background color:"]
+ const placeholderOfForm=["Untitled","1","","name"]
 
 const StoryDetails = () => {
- 
-  return (
-    
-<div>
 
+  const [Title, setTitle] = useState("");
+  const [NumberOfSlides, setNumberOfSlides] = useState("");
+  const [description , setdescription ] = useState("");
+  const [MainCharacters, setMainCharacters] = useState("");
+  const [Language, setLanguage] = useState("");
+  const [TargetAudiences, setTargetAudiences] = useState("");
+  const [uploadedImageUrl, setUploadedImageUrl] = useState(null);
+
+  let story = {}
+  const startWritingHandler = async () => {
+    story = {
+      Title,
+      NumberOfSlides,
+      description,
+      MainCharacters,
+      Language,
+      TargetAudiences,
+      Categorys
+    }
+    // const res = await account("story", story)
+}
+
+ const [Categorys,setCategorys]=useState([]);
+ const handleChange = (event) => {
+  const selectedCategory = event.target.value;
+  setCategorys([...Categorys, selectedCategory]);
+  displayCategorys(selectedCategory)
+};
+
+
+  const handleDelete = (indexToRemove, event) => {
+    event.preventDefault();
+    setCategorys(() => {
+      let newCategorys = [...Categorys];
+      newCategorys.splice(indexToRemove, 1);
+      return newCategorys;
+    });
+  };
+
+const displayCategorys = () => {
+  return Categorys.map((category, index) => (
+    <div className="displayDiv" key={index}>
+    <p className="pInBorder" >{category} </p>
+    <button onClick={(event) => handleDelete(index,event)} className="buttonInCategory">X</button>
+    </div> 
+  ));
+};
+
+  return (
+<div>
   <div className="container">
   <p className="parg">story Details</p>
   <div className="container story">
-  <div className="container">
-  <div className="container">
 <form className="row g-3">
   <div className="col-md-6">
-  <label htmlFor="inputCity" className="form-label">Title</label>
-  <input type="text" className="form-control" id="inputCity" placeholder="Untitled" />
+  <LabelOfStory htmlFor={htmlForStory[0]} type={typeofStory[0]} name={nameOfDetails[0]} placeholder={placeholderOfForm[0]} method={setTitle}/>
   </div>
+  
   <div className="col-md-6 justify-content-start NumberOfSlide">
-    <label htmlFor="inputPassword4" className="form-label">Number of slides</label>
-    <input  type="number" className="form-control" name="num-slides" defaultValue={1} />
+  <LabelOfStory htmlFor={htmlForStory[1]} type={typeofStory[1]} name={nameOfDetails[1]} placeholder={placeholderOfForm[1]} method={setNumberOfSlides}/>
   </div>
-  <div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">description (300 word max)</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  
+  <div className="mb-3">
+  <LabelOfStory htmlFor={htmlForStory[2]} type={typeofStory[3]} name={nameOfDetails[2]} method={setdescription}/>
 </div>
 
   <div className="col-6">
-    <label htmlFor="inputAddress2" className="form-label">Main Characters</label>
-    <input type="text" className="form-control" id="inputAddress2" placeholder="Name" />
+  <LabelOfStory htmlFor={htmlForStory[3]} type={typeofStory[0]} name={nameOfDetails[3]} placeholder={placeholderOfForm[3]}method={setMainCharacters}/>
 </div>
 
-<Languege/>
-<TargetAudience/> 
-<Category/>
+<Languege  method={setLanguage}/>
+<TargetAudience method={setTargetAudiences}/> 
+<Category handleChange={handleChange} displayCategorys={displayCategorys} method={setCategorys}/>
 
     <div className="col-md-3">
-  <label className="back" htmlFor="character-background-color">Background color:</label>
-  <input type="color" id="character-background-color" name="character-background-color" />
-
+    <LabelOfStory htmlFor={htmlForStory[4]} type={typeofStory[2]} name={nameOfDetails[4]} method={setTitle}/>
   </div>
-<div className="col-md-6 Add-cover">
-<button className="button button1">Add Cover</button>
-</div>
 
+  <div className="col-md-6 Add-cover">
+              {uploadedImageUrl ? (
+                <img src={uploadedImageUrl} alt="Uploaded Cover" className="uploaded-cover" />
+              ) : (
+                <UploadButton uploader={uploader}
+                  options={options}
+                  onComplete={files => {
+                    setUploadedImageUrl(files[0].fileUrl);
+                    alert(files.map(x => x.fileUrl).join("\n"));
+                  }}>
+                  {({ onClick }) =>
+                    <button className="button1" onClick={onClick}>
+                      Add Cover
+                    </button>
+                  }
+                </UploadButton>
+              )}
+            </div>
 
   <div>
-  <button className="button2"  classname="btn btn-primary btn-lg " >Start Writing</button>
-  <button className="cancel-button" classname="btn btn-secondary btn-lg " >Cancel</button>
+  <button id="button2" className=" btn btn-primary btn-lg " onClick={() => startWritingHandler()} >Start Writing</button>
+  <button id="cancel-button" className=" btn btn-secondary btn-lg" >Cancel</button>
 </div>
-   
-
-
-
 
 <div>
 </div>
@@ -64,14 +127,6 @@ const StoryDetails = () => {
 </div>
 </div>
 </div>
-</div>
-</div>
-
   )
 }
-
 export default StoryDetails;
-
-
-
-
