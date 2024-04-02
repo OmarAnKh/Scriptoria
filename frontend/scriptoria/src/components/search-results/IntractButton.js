0f772f7c634df5f7b0ab51fa4f84ad4a8f08f694
Ahmad <ahmadgggahmadggg55@gmail.com>
2024-04-02 06:18:26 +0300

    task done

--- frontend/scriptoria/src/components/search-results/IntractButton.js
+++ frontend/scriptoria/src/components/search-results/IntractButton.js
@@ -4,7 +4,7 @@ const ActionButton = (props) => {
     return (
         <button type="button" className={props.className} onClick={props.method}>
             {props.label}
-            <svg xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} fill="currentColor" viewBox={props.viewBox}>
+            <svg xmlns="http://www.w3.org/2000/svg" width={props.width} className={props.svgClassName} height={props.height} fill="currentColor" viewBox={props.viewBox}>
                 <path d={props.path1} />
                 <path d={props.path2} />
                 <path d={props.path3} />
