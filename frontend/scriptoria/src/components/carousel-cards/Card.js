450183e67f0d68e904c119d48714ba5e74005e8a
Omar Khalili <OmarKhalili810@gmail.com>
2024-04-23 13:27:00 +0300

    fixing the carouselcards css

--- frontend/scriptoria/src/components/carousel-cards/Card.js
+++ frontend/scriptoria/src/components/carousel-cards/Card.js
@@ -4,19 +4,19 @@ import { useTranslation } from 'react-i18next';
 const Card = ({ data }) => {
     const { t } = useTranslation();
     return (
-        <div className={`card mb-3 ${data.bgColor} mt-4 col-lg-12`} style={{ width: 750, maxHeight: '100%', height: 250 }}>
+        <div className={`CarouselCards mb-3 ${data.bgColor} mt-4 col-lg-12`} style={{ width: 750, maxHeight: '100%', height: 250 }}>
             <div className="row g-0">
                 <div className="col-md-4">
                     <img src={data.cover} className="cover-img img-fluid rounded-start" alt="..." style={{ maxWidth: '100%', maxHeight: 250, width: 180 }} />
                 </div>
                 <div className="col-md-8 d-none d-md-block">
-                    <div className={`card-body ${data.textColor}`}>
-                        <h4 className="card-title">{data.title}</h4>
+                    <div className={`CarouselCards-body ${data.textColor}`}>
+                        <h4 className="CarouselCards-title">{data.title}</h4>
                         <h6 className="author-name">{data.author}</h6>
                         <span className="d-flex">
                             <StarRating rating={data.rating} />  &emsp; {data.votes} votes
                         </span>
-                        <p className="card-text text-sm">{data.description}</p>
+                        <p className="CarouselCards-text text-sm">{data.description}</p>
                         <button type="button" className="btn btn-light rounded-4 px-5 fw-bold read-btn">{t("CarouselCards.read_the_book")}</button>
                     </div>
                 </div>
--- frontend/scriptoria/src/components/story-card/StoryCard.js
+++ frontend/scriptoria/src/components/story-card/StoryCard.js
@@ -4,7 +4,7 @@ import StarRating from '../star-rating/StarRating';
 import { useTranslation } from 'react-i18next';
 import { Buffer } from 'buffer';
 
-const StoryCard = ({selectedTab}) => {
+const StoryCard = ({ selectedTab }) => {
     const { t } = useTranslation()
     const [visiblestory, setVisiblestory] = useState(3);
     const loadMoreCards = () => {
@@ -13,26 +13,27 @@ const StoryCard = ({selectedTab}) => {
     const [storyData, setStoryData] = useState([]);
 
 
-  useEffect(() => {
-    console.log(selectedTab);
-    const fetchStoriesByGenre = async () => {
-      try {
-        const response = await fetch(`/storiesGenre/${selectedTab}`);
-        if (response.ok) {
-            const data = await response.json();
-            console.log( data);
-            setStoryData(data || []);
-        } else {
-          throw new Error('Failed to fetch stories');
-        }
-      } catch (error) {
-        console.error('Error fetching stories:', error);
-      }
-    };
+    useEffect(() => {
+        console.log(selectedTab);
+        const fetchStoriesByGenre = async () => {
+            try {
+                const response = await fetch(`/storiesGenre/${selectedTab}`);
+                
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log(data,100);
+                    setStoryData(data || []);
+                } else {
+                    throw new Error('Failed to fetch stories');
+                }
+            } catch (error) {
+                console.error('Error fetching stories:', error);
+            }
+        };
 
-    fetchStoriesByGenre();
-  }, [selectedTab]);
-   
+        fetchStoriesByGenre();
+    }, [selectedTab]);
+
     return (
         <div className=" d-flex flex-wrap">
             {storyData.map((story, index) => (
@@ -40,19 +41,19 @@ const StoryCard = ({selectedTab}) => {
                     <div className={`card  mb-1`} style={{ width: '95%', height: '100%', margin: '0 10px', minHeight: '200px', backgroundColor: story.story.backgroundColor }}>
                         <div className="row no-gutters">
                             <div className="col-md-4">
-                            <img 
-                                src={story.story.coverPhoto?.data ? `data:image/jpeg;base64,${Buffer.from(story.story.coverPhoto.data).toString('base64')}` : ''} 
-                                alt="story-img" 
-                                className="card-img" 
-                                style={{ width: '100%', height: '100%', minHeight: '200px' }}   />
+                                <img
+                                    src={story.story.coverPhoto?.data ? `data:image/jpeg;base64,${Buffer.from(story.story.coverPhoto.data).toString('base64')}` : ''}
+                                    alt="story-img"
+                                    className="card-img"
+                                    style={{ width: '100%', height: '100%', minHeight: '200px' }} />
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
                                     <h5 className="card-title story-title text-white">{story.story.title}</h5>
-                                    <h6 className="author text-white">{story.accounts[0].displayName}</h6>
+                                    <h6 className="author text-white">{console.log(story)}</h6>
                                     <div className="d-flex">
                                         <StarRating rating={story.counts.avg} />
-                                        <div className='voters text-white'>{story.counts.rates } votes</div>
+                                        <div className='voters text-white'>{story.counts.rates} votes</div>
                                     </div>
                                     <p className="card-text description-story text-white">{story.story.description}</p>
                                 </div>
@@ -66,6 +67,6 @@ const StoryCard = ({selectedTab}) => {
             )}
         </div>
     );
-    
+
 };
 export default StoryCard;
