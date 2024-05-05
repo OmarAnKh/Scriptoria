524c4c063c44b96c20a1de8227dc994b89d79672
Omar Khalili <OmarKhalili810@gmail.com>
2024-05-05 20:25:52 +0300

    adding the RegExp for the search result

--- backend/src/routers/story.js
+++ backend/src/routers/story.js
@@ -62,11 +62,11 @@ router.get("/search/:criteria", async (req, res) => {
     try {
         const account = await Account.findOne({
             $or: [
-                { 'email': criteria },
-                { 'displayName': criteria },
-                { 'userName': criteria }
+                { 'email': { $regex: new RegExp(criteria, 'i') } },
+                { 'displayName': { $regex: new RegExp(criteria, 'i') } },
+                { 'userName': { $regex: new RegExp(criteria, 'i') } }
             ]
-        })
+        });
         let storyId
         if (account) {
             const writers = await Writers.find(
@@ -80,12 +80,12 @@ router.get("/search/:criteria", async (req, res) => {
 
         const stories = await Story.find({
             $or: [
-                { 'genres': criteria },
-                { 'title': criteria },
-                { 'language': criteria },
-                { '_id': storyId },
+                { 'genres': { $regex: new RegExp(criteria, 'i') } },
+                { 'title': { $regex: new RegExp(criteria, 'i') } },
+                { 'language': { $regex: new RegExp(criteria, 'i') } },
+                { '_id': storyId }
             ]
-        })
+        });
 
         if (stories.length === 0) {
             return res.status(404).send({ error: "could not find stories", status: false });
