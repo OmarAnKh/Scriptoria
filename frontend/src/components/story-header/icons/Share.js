4eaa451ec1a6fe69be0ae2e43cd96660808aa7dc
Omar Khalili <OmarKhalili810@gmail.com>
2024-05-25 12:41:30 +0300

    fixing the share button bug

--- frontend/src/components/story-header/icons/Share.js
+++ frontend/src/components/story-header/icons/Share.js
@@ -1,16 +1,20 @@
 import toast from "react-hot-toast"
-const Share = ({t}) => {
+const Share = ({ t }) => {
     const shareHandler = async () => {
         try {
-            await navigator.share({
-                title: document.title,
-                url: window.location.href
-            });
+            if (navigator.share) {
+                await navigator.share({
+                    title: document.title,
+                    url: window.location.href
+                })
+            } else {
+                await navigator.clipboard.writeText(window.location.href)
+                toast.success("Link copied to clipboard!")
+            }
         } catch (error) {
-            toast.error("could'nt copy the link.")
+            toast.error("Couldn't share the link.")
         }
-    };
-
+    }
     return (
         <span className="mx-5">
             <span className={`px-2 rounded heading ms-auto d-flex flex-column align-items-center`} onClick={shareHandler}>
